370f0785c7fe7cdd1093aeefd3ac87a4125579c4
Jacob D. Castro <jdcastro.business@gmail.com>
2020-01-19 02:40:20 -0800

    add working webcam

--- src/pages/PhotoView.js
+++ src/pages/PhotoView.js
@@ -1,25 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef } from 'react';
 import Webcam from 'react-webcam';
 import { Container } from '@material-ui/core';
 
-const PhotoView = () => {
-  const [viewState, setViewState] = useState({
-    height: 320,
-    width: 0,
-    streaming: false,
-    video: null,
-    canves: null,
-    photo: null,
-    startButton: null,
-  });
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: { exact: 'environment' },
+};
 
-  useEffect(() => {}, []);
+const PhotoView = () => {
+  const webcamRef = useRef(null);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc);
+  }, [webcamRef]);
 
   return (
     <Container id="camera">
       <Container maxWidth="md">
-        <div style={{ padding: '0', border: '4px solid red' }}>
-          <Webcam />
+        <div style={{ padding: '0', margin: '0' }}>
+          <Webcam
+            audio={false}
+            screenshotFormat="image/webp"
+            screenshotQuality={0.7}
+            // videoConstraints={videoConstraints}
+          />
+          <button
+            style={{
+              height: '45px',
+              width: '45px',
+              borderRadius: '100%',
+              backgroundColor: 'red',
+              border: '6px solid #dddddd',
+            }}
+            onClick={() => capture()}
+          ></button>
         </div>
       </Container>
     </Container>
