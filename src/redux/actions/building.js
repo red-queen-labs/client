20b6a2bf26fd8d2c05efcd9a22722dabca888745
Snowful <lolitsaburrito@gmail.com>
2020-01-18 23:16:31 -0800

    Redux action configuration by Ben

--- src/redux/actions/building.js
+++ src/redux/actions/building.js
@@ -4,19 +4,49 @@ import {
   REMOVE_BUILDING_SUCCESS,
   REMOVE_BUILDING_FAIL,
 } from '../actionTypes';
+import axios from 'axios';
 
-export const addBuildingToList = data => {
-  return {
-    type: ADD_BUILDING_SUCCESS,
-    payload: {
-      ...data,
-    },
-  };
+export const addBuildingToList = data => async dispatcher => {
+// SKIPPED -Ben
+	const { building } = data;
+
+	const body = JSON.stringify({
+		building
+	});
+
+	try {
+		const res = await axios.post('/api/profiles/save/building', body, config);
+		dispatcher({
+			type: ADD_BUILDING_SUCCESS,
+			payload: res,
+		});
+	} catch (e) {
+		dispatcher({
+			type: ADD_BUILDING_FAIL,
+			payload: e
+		})
+	}
+	
+	
 };
 
-export const removeBuildingFromList = data => {
-  return {
-    type: REMOVE_BUILDING_SUCCESS,
-    payload: id,
-  };
+export const removeBuildingFromList = data => async dispatcher => {
+  const { building } = data;
+
+	const body = JSON.stringify({
+		building
+	});
+
+	try {
+		const res = await axios.post('/api/profiles/save/building', body, config);
+		dispatcher({
+			type: ADD_BUILDING_SUCCESS,
+			payload: res,
+		});
+	} catch (e) {
+		dispatcher({
+			type: ADD_BUILDING_FAIL,
+			payload: e
+		})
+	}
 };
